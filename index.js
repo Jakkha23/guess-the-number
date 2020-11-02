1cc4bb7f3f8da50e956f1a8655c8cc546c191436
Jakkha23 <jakob.khaytan@skola.botkyrka.se>
2020-11-02 15:20:14 +0100

    function added (not working)

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Read instructions. And remove this line.
+const realNumber = 34
+
+let guess = document.getElementById("user-input").Value
+
+
+
+function closeOrNot(guess) {
+    if (guess < realNumber) {
+        return "too low!"
+    }
+    if (guess > realNumber) {
+        return "too high!"
+    }
+    if (guess = realNumber) {
+        return "You guessed the right number!"
+    }
+}
+
+document.getElementById("user-input").onkeyup = closeOrNot
+
+
+console.log(closeOrNot(guess))
+
