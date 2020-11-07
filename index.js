bee6288552ccb37f433f8856a3a472e53e74fa93
Jakkha23 <jakob.khaytan@skola.botkyrka.se>
2020-11-07 15:27:12 +0100

    b

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-function getUserGuess () {
-    const stringvalue = document.getElementById('user-input').nodeValue
-    return parseInt(stringvalue, 10)
+//gör ett random nummer
+function randomInt() {
+    return Math.floor(Math.random() * 100)
 }
 
-document.addEventListener('keyup', function(event) {
+
+//secretNumber = randomnummer och loggar secret number
+const secretNumber = randomInt
+console.log(secretNumber())
+
+
+/*tar user guess från "user-input" i html filen och ger den namnet
+"stringvalue", "stringvalue" konverteras sedan till ett number*/
+function getUserGuess() {
+    const stringValue = document.getElementById('user-input').value
+    return parseInt(stringValue, 10)
+}
+
+
+/*
+lyssnar på eventet "keyup" på Enter knappen och deklarerar sedan "guess" som det som 
+användaren skrev in i "user-input" fast konverterat till en siffra
+*/
+document.addEventListener('keyup', function (event) {
     if (event.key === 'Enter') {
-        const guess = getGuess(
-            console.log(guess)
-        )
+        const guess = getUserGuess()
+        console.log(guess)
     }
 })
 
-function setMessage(msg) {
-    if (guess < realNumber) {
-        return "too low!"
-    }
-    if (guess > realNumber) {
-        return "too high!"
-    }
-    if (guess = realNumber) {
-        return "You guessed the right number!"
-    }
+if (guess < secretNumber) {
+    setMessage('too  low!')
 }
+if (guess > secretNumber) {
+    setMessage('too high!')
+}
+if (guess === secretNumber) {
+    setMessage('right answer!')
+}
+function setMessage(msg) {
+    document.getElementById("message").innerText = msg
+}
+
+
+
 
